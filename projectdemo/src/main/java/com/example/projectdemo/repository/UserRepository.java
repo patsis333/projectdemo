1f50d279d3a38f55b50640b266703fda80928217
patsis333 <86531088+patsis333@users.noreply.github.com>
2021-09-10 11:39:04 +0200

    Delete projectdemo directory

--- projectdemo/src/main/java/com/example/projectdemo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectdemo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.projectdemo.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	
-}
